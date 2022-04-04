7f7cd340e5634e3939ea7297b3e4e6282f3cb2a2
Lucas Vinicio Alves Almeida <lucasvinicio@ucl.br>
2022-04-04 13:15:01 -0300

    função que realiza potenciação

--- codigo.c
+++ codigo.c
@@ -1,13 +1,21 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+int Potencia(int base, int expoente)
+{
+    if(expoente == 0)
+        return 1;
+    else
+        return (base * Potencia(base, expoente-1));
+}
+
 int main(void)
 {
-    int bin1, bin2;
+    int bin1, bin2, dec=0;
     char operacao;
     scanf("%c", &operacao);
     scanf("%d", &bin1);
     scanf("%d", &bin2);
-    
+
     return 0;
 }
