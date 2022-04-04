fc649dce508082675bd338d32d78275207a6d5b6
Lucas Vinicio Alves Almeida <lucasvinicio@ucl.br>
2022-04-04 13:02:32 -0300

    criando as entradas

---
+++ codigo.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(void)
+{
+    int bin1, bin2;
+    char operacao;
+    scanf("%c", &operacao);
+    scanf("%d", &bin1);
+    scanf("%d", &bin2);
+    
+    return 0;
+}
