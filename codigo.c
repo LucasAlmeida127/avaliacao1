92902ebc63b618060267571f2e4858f8b1389474
Lucas Vinicio Alves Almeida <lucasvinicio@ucl.br>
2022-04-04 13:39:48 -0300

    conversão binário para decimal

--- codigo.c
+++ codigo.c
@@ -9,13 +9,26 @@ int Potencia(int base, int expoente)
         return (base * Potencia(base, expoente-1));
 }
 
+int TransformaDec(int binario)
+{
+    int decimal = 0, i;
+    for(i = 0; binario > 0; i++)
+    {
+        decimal += Potencia(2,i) * (binario % 10);
+        binario /= 10;
+    }
+    return decimal;
+}
+
 int main(void)
 {
     int bin1, bin2, dec=0;
     char operacao;
-    scanf("%c", &operacao);
+    //scanf("%c", &operacao);
     scanf("%d", &bin1);
-    scanf("%d", &bin2);
+    //scanf("%d", &bin2);
+    dec = TransformaDec(bin1);
+    printf("Valor em decimal: %d",dec);
 
     return 0;
 }
